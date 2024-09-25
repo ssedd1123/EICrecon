ba3d4594fa641c7b663ffbb933a3faf12871b4b6
ssedd1123 <tommytsang3108@hotmail.com.hk>
2024-09-25 18:03:29 -0400

    Added RecordCellHitsProcessor

---
+++ RecordCellHits/RecordCellHitsProcessor.cc
@@ -0,0 +1,106 @@
+
+//
+// Template for this file generated with eicmkplugin.py
+//
+
+#include "RecordCellHitsProcessor.h"
+#include "services/rootfile/RootFile_service.h"
+#include "services/geometry/dd4hep/DD4hep_service.h"
+
+// Include appropriate class headers. e.g.
+#include <edm4eic/RawTrackerHitCollection.h>
+#include <edm4eic/TrackerHitCollection.h>
+#include <edm4hep/SimTrackerHitCollection.h>
+
+// The following just makes this a JANA plugin
+extern "C" {
+    void InitPlugin(JApplication *app) {
+        InitJANAPlugin(app);
+        app->Add(new RecordCellHitsProcessor);
+    }
+}
+
+//-------------------------------------------
+// InitWithGlobalRootLock
+//-------------------------------------------
+void RecordCellHitsProcessor::InitWithGlobalRootLock(){
+    auto rootfile_svc = GetApplication()->GetService<RootFile_service>();
+    auto rootfile = rootfile_svc->GetHistFile();
+    rootfile->mkdir("RecordCellHits")->cd();
+
+    // Create histograms here. e.g.
+    _tree = new TTree("cellHits", "ADC TDC hits for all cells");
+    _tree -> Branch("eventID", &_eventID);
+    _tree -> Branch("cellID", &_cellID);
+    _tree -> Branch("TDC", &_TDC);
+    _tree -> Branch("ADC", &_ADC);
+    _tree -> Branch("x", &_x);
+    _tree -> Branch("y", &_y);
+    _tree -> Branch("z", &_z);
+    _tree -> Branch("simx", &_simx);
+    _tree -> Branch("simy", &_simy);
+    _tree -> Branch("simz", &_simz);
+    _tree -> Branch("simc", &_simc);
+
+    // Create histograms here. e.g.
+    _detector = GetApplication() -> GetService<DD4hep_service>() -> detector();
+    _converter = std::make_unique<dd4hep::rec::CellIDPositionConverter>(*_detector);
+}
+
+//-------------------------------------------
+// ProcessSequential
+//-------------------------------------------
+void RecordCellHitsProcessor::ProcessSequential(const std::shared_ptr<const JEvent>& event) {
+    // Data objects we will need from JANA e.g.
+    const auto &truehits = *(event->GetCollection<edm4hep::SimTrackerHit>("TOFBarrelHits"));
+    _eventID = event -> GetEventNumber();
+    // this script only works when there is only one hit per event
+    // Because I don't want to perform any hit association for simplicity
+    if(truehits.size() != 1) 
+        return;
+    for(auto hit : truehits) {
+        auto pos = hit.getPosition();
+	_simx = pos.x;
+	_simy = pos.y;
+	_simz = pos.z;
+	_simc = hit.getEDep();
+    }
+
+    _cellID.clear();
+    _TDC.clear();
+    _ADC.clear();
+    _x.clear();
+    _y.clear();
+    _z.clear();
+    const auto &rawhits = *(event->GetCollection<edm4eic::RawTrackerHit>("TOFBarrelADCTDC"));
+
+    for(auto hit: rawhits) {
+	auto cellID = hit.getCellID();
+        auto pos = _converter -> position(cellID);
+
+	_cellID.push_back(cellID);
+	_ADC.push_back(hit.getCharge());
+        _TDC.push_back(hit.getTimeStamp());
+	_x.push_back(pos.X());
+	_y.push_back(pos.Y());
+	_z.push_back(pos.Z());
+    }
+
+    _tree -> Fill();
+
+}
+
+//-------------------------------------------
+// FinishWithGlobalRootLock
+//-------------------------------------------
+void RecordCellHitsProcessor::FinishWithGlobalRootLock() {
+
+    // Do any final calculations here.
+    //auto rootfile_svc = GetApplication()->GetService<RootFile_service>();
+    //auto rootfile = rootfile_svc->GetHistFile();
+    //rootfile -> cd("RecordCellHits");
+
+    //_tree -> Write();
+
+}
+
---
+++ RecordCellHits/RecordCellHitsProcessor.h
@@ -0,0 +1,38 @@
+
+//
+// Template for this file generated with eicmkplugin.py
+//
+
+#include <JANA/JEventProcessorSequentialRoot.h>
+#include <TH2D.h>
+#include <TFile.h>
+#include <TTree.h>
+
+#include <DD4hep/Detector.h>
+#include <DDRec/CellIDPositionConverter.h>
+
+class RecordCellHitsProcessor: public JEventProcessorSequentialRoot {
+private:
+
+    // Declare histogram and tree pointers here. e.g.
+    // TH1D* hEraw  = nullptr;
+    // TH2D* hEdigi = nullptr ;
+    TTree *_tree = nullptr;
+    std::vector<dd4hep::rec::CellID> _cellID;
+    std::vector<double> _TDC;
+    std::vector<double> _ADC;
+    std::vector<double> _x, _y, _z;
+    double _simx, _simy, _simz;
+    double _simc;
+    int _eventID;
+
+    const dd4hep::Detector* _detector                     = nullptr;
+    std::unique_ptr<dd4hep::rec::CellIDPositionConverter> _converter;
+
+public:
+    RecordCellHitsProcessor() { SetTypeName(NAME_OF_THIS); }
+
+    void InitWithGlobalRootLock() override;
+    void ProcessSequential(const std::shared_ptr<const JEvent>& event) override;
+    void FinishWithGlobalRootLock() override;
+};
